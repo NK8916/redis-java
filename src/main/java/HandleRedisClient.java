aa84f6cadab272d800edaa29d5f77af3c9cd30e0
Nitin Kumar <2018kumarnitin@gmail.com>
2025-05-29 09:42:59 +0530

    add set or get map values

---
+++ src/main/java/HandleRedisClient.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class HandleRedisClient {
+    private static HashMap<String, String> map=new HashMap<>();
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+
+    HandleRedisClient(Socket client) throws IOException {
+        System.out.println("Client connected: " + client.getInetAddress().getHostAddress());
+        this.reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+        this.writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
+    }
+
+    public void handleClient() throws IOException {
+        String line;
+        while((line = reader.readLine())!=null){
+            System.out.println("Received request: " + line);
+            if(line.startsWith("*")){
+                int numArgs = Integer.parseInt(line.substring(1));
+                reader.readLine();
+                line= reader.readLine();
+                String command=line;
+                if(numArgs==1){
+                    if ("ping".equalsIgnoreCase(command)) {
+                        this.sendResponseToClient("PONG");
+                    }
+                }
+                else if((numArgs==3 && "set".equalsIgnoreCase(command)) || (numArgs==2 && "get".equalsIgnoreCase(command))){
+                    this.setOrGetKeyValPair((numArgs==3 && "set".equalsIgnoreCase(command)));
+                }
+                else{
+                    System.out.println(command);
+                    if("echo".equalsIgnoreCase(command)){
+                        this.echoResponse(numArgs);
+                    }
+                }
+            }
+        }
+    }
+
+    private void setOrGetKeyValPair(boolean setValue) throws IOException {
+        System.out.println("setValue+ "+setValue);
+        String line= this.reader.readLine();
+        System.out.println(line);
+        String key= reader.readLine();
+        if(setValue){
+            line= this.reader.readLine();
+            System.out.println(line);
+            String val= reader.readLine();
+            map.put(key,val);
+            System.out.println("value: "+map.get(key));
+        }
+        System.out.println("key: "+key);
+        String response=map.getOrDefault(key, "Key Value Pair does not exists");
+        System.out.println("response: "+response);
+        this.sendResponseToClient(response);
+    }
+
+    private void echoResponse(int numArgs) throws IOException {
+        ArrayList<String> resultList=new ArrayList<>();
+        String line;
+        for(int i=0;i<numArgs-1;i++){
+            line= reader.readLine();
+            System.out.println(line);
+            resultList.add(reader.readLine());
+        }
+        this.sendResponseToClient(String.join(" ",resultList));
+    }
+
+    private void sendResponseToClient(String text) throws IOException {
+        String result="+"+text+"\r\n";
+        this.writer.write(result);
+        this.writer.flush();
+        System.out.println("Sent response to client: "+result);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+    static HashMap<String, String> map= new HashMap<>();
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -20,7 +21,8 @@ public class Main {
                 ExecutorService threadPool = Executors.newCachedThreadPool();
                 threadPool.submit(()-> {
                     try {
-                        handleClient(client);
+                        HandleRedisClient handleRedisClient=new HandleRedisClient(client);
+                        handleRedisClient.handleClient();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -31,59 +33,68 @@ public class Main {
         }
   }
 
-  private static void handleClient(Socket client) throws IOException {
-      System.out.println("Client connected: " + client.getInetAddress().getHostAddress());
-      BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
-
-      String line;
-
-      while((line = reader.readLine())!=null){
-          System.out.println("Received request: " + line);
-          if(line.startsWith("*")){
-              int numArgs = Integer.parseInt(line.substring(1));
-              if(numArgs==1){
-                  line= reader.readLine();
-                  System.out.println(line);
-                  line= reader.readLine();
-                  String command=line;
-                  if ("ping".equalsIgnoreCase(command)) {
-                      writer.write("+PONG\r\n");
-                      writer.flush();
-                      System.out.println("Sent response to client: +PONG");
-                  }
-              }else{
-                  line= reader.readLine();
-                  System.out.println(line);
-                  line= reader.readLine();
-                  String command=line;
-                  System.out.println(command);
-                  if("echo".equalsIgnoreCase(command)){
-                      ArrayList<String> resultList=new ArrayList<>();
-                    for(int i=0;i<numArgs-1;i++){
-                        line= reader.readLine();
-                        System.out.println(line);
-                        resultList.add(reader.readLine());
-                    }
-                    String result="+"+String.join(" ",resultList)+"\r\n";
-                    writer.write(result);
-                    writer.flush();
-                    System.out.println("Sent response to client: "+result);
-                  }
-
-              }
-//              for(int i=0;i<numArgs;i++){
-//                  line= reader.readLine();
-//                  line= reader.readLine();
-//                  String command=line;
-//                  System.out.println("command: "+command);
+//  private static void handleClient(Socket client) throws IOException {
+//      System.out.println("Client connected: " + client.getInetAddress().getHostAddress());
+//      BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+//      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
+//
+//      String line;
+//
+//      while((line = reader.readLine())!=null){
+//          System.out.println("Received request: " + line);
+//          if(line.startsWith("*")){
+//              int numArgs = Integer.parseInt(line.substring(1));
+//              reader.readLine();
+//              line= reader.readLine();
+//              String command=line;
+//              if(numArgs==1){
 //                  if ("ping".equalsIgnoreCase(command)) {
 //                      writer.write("+PONG\r\n");
 //                      writer.flush();
 //                      System.out.println("Sent response to client: +PONG");
 //                  }
 //              }
-          }
-      }
-  }
+//              else if(numArgs==3 && "set".equalsIgnoreCase(command)){
+//                    line= reader.readLine();
+//                    System.out.println(line);
+//                    String key= reader.readLine();
+//                    line= reader.readLine();
+//                    System.out.println(line);
+//                    String val= reader.readLine();
+//                    map.put(key,val);
+//                    String response=map.get(key);
+//                  String result="+"+response+"\r\n";
+//                  writer.write(result);
+//                  writer.flush();
+//                  System.out.println("Sent response to client: "+result);
+//
+//
+//              } else if (numArgs==2 && "get".equalsIgnoreCase(command)){
+//                  line= reader.readLine();
+//                  System.out.println(line);
+//                  String key= reader.readLine();
+//                  String response=map.get(key);
+//                  String result="+"+response+"\r\n";
+//                  writer.write(result);
+//                  writer.flush();
+//                  System.out.println("Sent response to client: "+result);
+//              }
+//              else{
+//                  System.out.println(command);
+//                  if("echo".equalsIgnoreCase(command)){
+//                      ArrayList<String> resultList=new ArrayList<>();
+//                    for(int i=0;i<numArgs-1;i++){
+//                        line= reader.readLine();
+//                        System.out.println(line);
+//                        resultList.add(reader.readLine());
+//                    }
+//                    String result="+"+String.join(" ",resultList)+"\r\n";
+//                    writer.write(result);
+//                    writer.flush();
+//                    System.out.println("Sent response to client: "+result);
+//                  }
+//              }
+//          }
+//      }
+//  }
 }
