26b5a7eb6627dcd105459c5e5142870aa996a4bd
Nitin Kumar <2018kumarnitin@gmail.com>
2025-05-31 16:42:32 +0530

    add null string

--- src/main/java/HandleRedisClient.java
+++ src/main/java/HandleRedisClient.java
@@ -104,7 +104,7 @@ public class HandleRedisClient {
             this.sendResponseToClient("OK");
         }else{
             System.out.println("key: "+key);
-            String response=map.getOrDefault(key, "+$-1\r\n");
+            String response=map.getOrDefault(key, "$-1");
             System.out.println("response: "+response);
             this.sendResponseToClient(response);
         }
